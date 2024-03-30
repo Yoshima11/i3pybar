25995be8769ab9be09d06c37e55095f5ab1ec716
Yoshima11 <miked160581@gmail.com>
2024-03-30 13:20:46 -0300

    1.1

--- i3pybar.py
+++ i3pybar.py
@@ -4,6 +4,8 @@ import psutil
 
 disks = psutil.disk_partitions()
 cpuTempPath = '/sys/class/thermal/thermal_zone1/temp'
+gpu_temp_path = '/sys/class/drm/card1/device/hwmon/hwmon1/temp1_input'
+gpu_freq_path = '/sys/class/drm/card1/device/hwmon/hwmon1/freq1_input'
 locale.setlocale(locale.LC_TIME, 'es_ES.utf8')
 currentDate = time.strftime('%a %d de %b')
 currentTime = time.strftime('%H:%M')
@@ -15,6 +17,13 @@ file = open(cpuTempPath)
 cpu_temp = int(int(file.read()) / 1000)
 file.close()
 
+file = open(gpu_temp_path)
+gpu_temp = int(int(file.read()) / 1000)
+file.close()
+
+file = open(gpu_freq_path)
+gpu_freq = int(int(file.read())/100000)
+file.close()
 
 def color_disks(c_disk):
     if c_disk < 50:
@@ -83,6 +92,18 @@ def icon(i_cpu):
         return "\uf2c8"
     return "\uf2c7"
 
+def color_gpu(c_gpu):
+    if c_gpu < 40:
+        return "#00FF00"
+    if c_gpu < 50:
+        return "#bfff00"
+    if c_gpu < 60:
+        return "#ffff00"
+    if c_gpu < 70:
+        return "#ffbf00"
+    if c_gpu < 80:
+        return "#ff8000"
+    return "#ff0000"
 
 print_text = ""
 
@@ -91,13 +112,27 @@ for i in range(0, len(disks)):
         print_text = print_text + '\uf0a0 ' + disks[i].mountpoint + ':<span color="' + color_disks(
             int(psutil.disk_usage(disks[i].mountpoint).percent)) + '">' + str(
             psutil.disk_usage(disks[i].mountpoint).percent) + '%</span> '
-
+'''
 print(
     print_text +
     '\uf2db:<span color="{}">{}%</span> '
     '\uf0ae :<span color="{}">{}%</span> '
     '{}:<span color="{}">{:>2}ºC</span> '
+    'G :{:>2}Mhz '
+    '{}:<span color="{}">{:>2}ºC</span> '
     '\uf073 <span color="#BEBEBE">{}</span> '
     '\uf017 <span color="#BEBEBE">{}</span> '
     .format(color_mem(mem), mem, color_uso(cpu_uso), cpu_uso, icon(cpu_temp), color_temp(cpu_temp), cpu_temp,
-            currentDate, currentTime))
+            gpu_freq, icon(gpu_temp), color_gpu(gpu_temp), gpu_temp, currentDate, currentTime))
+'''
+print(
+    print_text +
+    'MEM:<span color="{}">{}%</span> '
+    'CPU :<span color="{}">{}%</span> '
+    '{}:<span color="{}">{:>2}ºC</span> '
+    'GPU :{:>2}Mhz '
+    '{}:<span color="{}">{:>2}ºC</span> '
+    '\uf073 <span color="#BEBEBE">{}</span> '
+    '\uf017 <span color="#BEBEBE">{}</span> '
+    .format(color_mem(mem), mem, color_uso(cpu_uso), cpu_uso, icon(cpu_temp), color_temp(cpu_temp), cpu_temp,
+            gpu_freq, icon(gpu_temp), color_gpu(gpu_temp), gpu_temp, currentDate, currentTime))
